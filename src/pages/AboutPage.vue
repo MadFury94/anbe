b55c00f76375ef8c466a81145ac923dd7cbd0cb9
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-06-10 18:17:00 +0100

    second area of about us page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -9,14 +9,49 @@
     </div>
     <div class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4">
       <div class="py-4 px-32">
-        <div class="w-14 border-2 border-orange-500 lg:pb-4"></div>
+        <div class="w-10 border-2 border-orange-500 lg:pb-4"></div>
       </div>
-      <h1 class="pb-10 text-3xl text-orange-500">ANBE NIGERIA LTD</h1>
+      <h1 class="pb-10 text-3xl text-orange-500">ABOUT US</h1>
       <p class="pb-7 font-light">
-        “We aim to become an international Pipeline, Engineering and
-        Construction service company that provides services of International
-        standard from brian”
+        “ANBE NIGERIA LTD is an indigenous Engineering company, incorporated in
+        1990 to provide prompt and efficient services to the Oil and Gas sectors
+        of the economy.
       </p>
+
+      <button
+        class="border border-orange-500 bg-transparent py-2 px-5 text-orange-500 hover:border-transparent hover:bg-orange-300"
+      >
+        VIEW OUR WOR
+      </button>
+    </div>
+  </div>
+  <div class="relative text-center font-bold text-white">
+    <div class="w-full brightness-50">
+      <img
+        class="h-auto lg:h-[800px] w-full object-fill"
+        src="https://anbenig.com/wp-content/uploads/2019/12/construction-35.jpg"
+        alt="Snow"
+      />
+    </div>
+    <div class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4">
+      <h1>The major areas of Competence are:</h1>
+      <div class="font-light">
+        <ul>
+          Design and fabrication of Smokeless
+        </ul>
+        <ul>
+          Flares Pipeline installation
+        </ul>
+        <ul>
+          repair and maintenance Pipeline corrosion
+        </ul>
+        <ul>
+          leak repair Remote Ignition systems Instrumentation Leasing
+        </ul>
+        <ul>
+          Maintenance of operation vehicles Manpower supplies
+        </ul>
+      </div>
 
       <button
         class="border border-orange-500 bg-transparent py-2 px-5 text-orange-500 hover:border-transparent hover:bg-orange-300"
