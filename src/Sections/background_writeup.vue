41fba4e3d2da77953c3409f91f239c542d15617e
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-06-14 10:02:54 +0100

    about us revision

---
+++ src/Sections/background_writeup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="relative text-center font-bold text-white">
+      <div class="w-full brightness-50">
+        <img
+          class="h-auto lg:h-[800px] w-full object-fill"
+          src="https://anbenig.com/wp-content/uploads/2019/12/construction-35.jpg"
+          alt="Snow"
+        />
+      </div>
+      <div class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4">
+        <div class="py-4 px-32">
+          <div class="w-14 border-2 border-orange-500 lg:pb-4"></div>
+        </div>
+        <h1 class="pb-10 text-3xl text-orange-500">ANBE NIGERIA LTD</h1>
+        <p class="pb-7 font-light">
+          “We aim to become an international Pipeline, Engineering and
+          Construction service company that provides services of International
+          standard from brian”
+        </p>
+
+        <button
+          class="border border-orange-500 bg-transparent py-2 px-5 text-orange-500 hover:border-transparent hover:bg-orange-300"
+        >
+          VIEW OUR WOR
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,84 +1,15 @@
+<script setup>
+import Background_writeup from "../Sections/background_writeup.vue";
+import Backgroundimage_button from "../Sections/backgroundimage_button.vue";
+import Localcontent from "../Sections/localcontent.vue";
+import Ourleader from "../Sections/ourleader.vue";
+</script>
+
 <template>
-  <div class="relative text-center font-bold text-white">
-    <div class="w-full brightness-50">
-      <img
-        class="h-auto lg:h-[800px] w-full object-fill"
-        src="https://anbenig.com/wp-content/uploads/2019/12/handyman_10.jpg"
-        alt="Snow"
-      />
-    </div>
-    <div class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4">
-      <div class="py-4 px-32">
-        <div class="w-10 border-2 border-orange-500 lg:pb-1"></div>
-      </div>
-      <h1 class="pb-10 text-3xl text-orange-500">ABOUT US</h1>
-      <p class="pb-7 font-light">
-        “ANBE NIGERIA LTD is an indigenous Engineering company, incorporated in
-        1990 to provide prompt and efficient services to the Oil and Gas sectors
-        of the economy.
-      </p>
-
-      <button
-        class="border border-orange-500 bg-transparent py-2 px-5 text-orange-500 hover:border-transparent hover:bg-orange-300"
-      >
-        VIEW OUR WOR
-      </button>
-    </div>
-  </div>
-  <div class="relative text-center font-bold text-white">
-    <div class="py-10 bg-black">
-      <div>
-        <h1>The major areas of Competence are:</h1>
-      </div>
-    </div>
-    <div class="font-light bg-black">
-      <ul class="list-disc">
-        <li>Design and fabrication of Smokeless Flares</li>
-        <li>Pipeline installation Repair and maintenance</li>
-        <li>Pipeline corrosion Leak</li>
-        <li>Repair Remote Ignition systems Instrumentation</li>
-        <li>Leasing Maintenance of operation Vehicles Manpower supplies</li>
-      </ul>
-    </div>
-    <div class="bg-black py-5">
-      <p class="font-light px-3">
-        ANBE has a team of highly qualified and experienced personnel with
-        background in various engineering fields, specializing in combustion
-        technology and equipment maintenance, such as generators and earth
-        moving equipment. ANBE has a well equipped ultra modern Fabrication and
-        Maintenance facility with state of the art equipment to undertake
-        projects in her area of competence. We also have a corporate
-        administrative Office, well equipped for timely delivery of projects.
-        ANBE has a large pool of working capital and enjoys adequate financial
-        backing from first class banks in the country
-      </p>
-    </div>
-    <div class>
-      <div class="py-4 px-32">
-        <div class="w-10 border-2 border-orange-500 lg:pb-1"></div>
-        <h1 class="pb-10 text-3xl text-black">Our Leader</h1>
-        <p class="text-black">
-          Meet our Chief Executive Officer, Ernest Azukaeme.
-        </p>
-        <div>
-          <img
-            class=""
-            src="https://anbenig.com/wp-content/uploads/2020/01/12065477_10207241341167807_1876850073176580906_n.jpg"
-            alt="Ernest"
-          />
-          <p class="text-black px">Ernest Azukaeme</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <main>
+    <Background_writeup></Background_writeup>
+    <Backgroundimage_button></Backgroundimage_button>
+    <Localcontent></Localcontent>
+    <Ourleader></Ourleader>
+  </main>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
