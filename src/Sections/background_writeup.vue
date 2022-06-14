7b0302160f891ea167c7563c070f5f1331e1e37c
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-06-14 10:21:52 +0100

    about us building

--- src/Sections/background_writeup.vue
+++ src/Sections/background_writeup.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="relative text-center font-bold text-white">
+    <div class="relative text-center font-bold text-white py-5">
       <div class="w-full brightness-50">
         <img
-          class="h-auto lg:h-[800px] w-full object-fill"
-          src="https://anbenig.com/wp-content/uploads/2019/12/construction-35.jpg"
+          class="h-auto lg:h-[800px] w-full object-fill sm:h-[600px]"
+          src="https://anbenig.com/wp-content/uploads/2019/12/handyman_02.jpg"
           alt="Snow"
         />
       </div>
@@ -12,18 +12,28 @@
         <div class="py-4 px-32">
           <div class="w-14 border-2 border-orange-500 lg:pb-4"></div>
         </div>
-        <h1 class="pb-10 text-3xl text-orange-500">ANBE NIGERIA LTD</h1>
+        <h1 class="pb-10 text-3xl text-orange-500">ABOUT US</h1>
         <p class="pb-7 font-light">
-          “We aim to become an international Pipeline, Engineering and
-          Construction service company that provides services of International
-          standard from brian”
+          “ANBE NIGERIA LTD is an indigenous Engineering company, incorporated
+          in 1990 to provide prompt and efficient services to the Oil and Gas
+          sectors of the economy.
         </p>
 
-        <button
-          class="border border-orange-500 bg-transparent py-2 px-5 text-orange-500 hover:border-transparent hover:bg-orange-300"
-        >
-          VIEW OUR WOR
-        </button>
+        <div class="">
+          <div class="text-uppercase">
+            <h1>The major areas of Competence are:</h1>
+          </div>
+          <ul class="list-decimal">
+            <li>Design and fabrication of Smokeless Flares</li>
+            <li>Pipeline installation, repair and maintenance</li>
+            <li>Pipeline corrosion and leak repair</li>
+            <li>Remote Ignition systems</li>
+            <li>Instrumentation</li>
+            <li>Leasing and Maintenance of operation vehicles</li>
+            <li>Manpower supplies</li>
+          </ul>
+          <div class="py-10"></div>
+        </div>
       </div>
     </div>
   </div>
