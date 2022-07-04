969717889dcb65318c83731b7e5543b0f2b6a591
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-07-04 11:48:38 +0100

    latest

--- src/Sections/background_writeup.vue
+++ src/Sections/background_writeup.vue
@@ -3,27 +3,30 @@
     <div class="relative text-center font-bold text-white py-5">
       <div class="w-full brightness-50">
         <img
-          class="h-auto lg:h-[800px] w-full object-fill sm:h-[600px]"
+          class="h-auto lg:h-[800px]  object-fill sm:h-[800px]"
           src="https://anbenig.com/wp-content/uploads/2019/12/handyman_02.jpg"
           alt="Snow"
         />
       </div>
-      <div class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4">
-        <div class="py-4 px-32">
-          <div class="w-14 border-2 border-orange-500 lg:pb-4"></div>
+      <div
+        class="absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 py-4"
+      >
+        <div class="flex gap-x-4">
+          <div class="h-14 border-2 border-orange-500"></div>
+          <h1 class="text-3xl text-orange-500">ABOUT US</h1>
         </div>
-        <h1 class="pb-10 text-3xl text-orange-500">ABOUT US</h1>
-        <p class="pb-7 font-light">
+
+        <p class="pb- font-light">
           “ANBE NIGERIA LTD is an indigenous Engineering company, incorporated
           in 1990 to provide prompt and efficient services to the Oil and Gas
           sectors of the economy.
         </p>
 
         <div class="">
-          <div class="text-uppercase">
-            <h1>The major areas of Competence are:</h1>
+          <div>
+            <h1 class="">The major areas of Competence are:</h1>
           </div>
-          <ul class="list-decimal">
+          <ul class="list-disc font-light">
             <li>Design and fabrication of Smokeless Flares</li>
             <li>Pipeline installation, repair and maintenance</li>
             <li>Pipeline corrosion and leak repair</li>
@@ -32,7 +35,20 @@
             <li>Leasing and Maintenance of operation vehicles</li>
             <li>Manpower supplies</li>
           </ul>
-          <div class="py-10"></div>
+          <div class="py-10">
+            <p class="font-light">
+              ANBE has a team of highly qualified and experienced personnel with
+              background in various engineering fields, specializing in
+              combustion technology and equipment maintenance, such as
+              generators and earth moving equipment. ANBE has a well equipped
+              ultra modern Fabrication and Maintenance facility with state of
+              the art equipment to undertake projects in her area of competence.
+              We also have a corporate administrative Office, well equipped for
+              timely delivery of projects. ANBE has a large pool of working
+              capital and enjoys adequate financial backing from first class
+              banks in the country
+            </p>
+          </div>
         </div>
       </div>
     </div>
