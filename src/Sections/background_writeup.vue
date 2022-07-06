23f23a9425a7c02e4b8af851855b2edf241e16ac
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-07-06 19:40:43 +0100

    Our leader section

--- src/Sections/background_writeup.vue
+++ src/Sections/background_writeup.vue
@@ -127,30 +127,6 @@
         alt="Snow"
       />
     </div>
-    <div class="flex gap-x-4 py-4 px-4">
-      <div class="h-14 border-2 border-orange-500"></div>
-      <h1 class="text-3xl font-extrabold py-4">Our Leader</h1>
-    </div>
-    <p class="py-4 px-4">Meet our Chief Executive Officer, Ernest Azukaeme.</p>
-    <div class="py-4">
-      <img
-        class="object-contain h-48 w-96"
-        src="https://anbenig.com/wp-content/uploads/2020/01/12065477_10207241341167807_1876850073176580906_n.jpg"
-        alt="Leader"
-      />
-      <p class="py-4 px-24 font-semibold">Mr Ernest Azukaeme</p>
-      <p class="py-4 px-24 text-orange-500 font-semibold">Managing Director</p>
-      <p class="py-4 px-24 font-semibold">
-        Mr Ernest Azukaeme is the pioneer Managing Director of Jurak Oil
-        Services Ltd, an indigenous smokeless flare manufacturing company based
-        in Port Harcourt. A graduate of Business Administration with post
-        graduate qualification in marketing, Mr. Azukaeme’s experience traverses
-        the banking sector, where he spent over a decade specializing in risk
-        Asset Management. He worked for years as project Director with Walter
-        Smith Petromann Oil Company Ltd, company owned by Canadians / Nigerians,
-        which was involved in many projects for Shell and Chevron.
-      </p>
-    </div>
   </div>
 </template>
 <script setup></script>
