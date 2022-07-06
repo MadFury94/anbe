1450ab0eec3cc4df2b8638bc99d8313fdae0e4a0
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-07-06 19:42:40 +0100

    local area content

--- src/Sections/localcontent.vue
+++ src/Sections/localcontent.vue
@@ -1,4 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <div
+      class="grid gap-4 bg-gray-800 py-4 px-24 font-sans text-3xl font-bold uppercase text-orange-500 md:grid-cols-2"
+    >
+      Local Content Policy
+      <p class="text-sm font-extralight text-white">
+        It is the intention of ANBE NIGERIA LIMITED to encourage the purchase of
+        equipment, goods, materials, suppliers and manpower from Nigerian and to
+        make use of services and facilities professional or otherwise which are
+        rendered by Nigerians or companies incorporated in Nigeria, except when
+        after proper research such services/facilities are not available in
+        Nigeria. The ANBE NIGERIA LIMITED has established an independent
+        Nigerian Local Content Department to both collate and decipher
+        information on the state of affairs of local content in the Nigeria and
+        to monitor, audit and ensure compliance with planned Local Content
+        Objectives.
+      </p>
+    </div>
+  </div>
 </template>
 <script setup></script>
