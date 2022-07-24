1d34ed37f65657214c5d744e8fb12b5a356281e7
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-07-24 13:40:42 +0100

    keepinmind

---
+++ src/Sections/KeepInMind.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-gray-800 p-10 child">
+    <h1 class="font-bold text-white text-3xl">Keep in mind</h1>
+  </div>
+
+  <div class="grid grid-cols-2 gap-10 py-10">
+    <div class="h-40 w-40 border-4 border-orange-500 text-orange-500">
+      65 Happy Clients
+    </div>
+    <div class="border-4 border-orange-500 text-orange-500">
+      165 completed Projects
+    </div>
+    <div class="border-4 border-orange-500 text-orange-500">
+      3 Office Locations
+    </div>
+    <div class="border-4 border-orange-500 text-orange-500">CLICK HERE</div>
+  </div>
+</template>
