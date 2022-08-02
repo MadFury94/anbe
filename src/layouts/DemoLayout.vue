a241489ef8f24fc72cbcdfc2d158cdc761529196
katalyst33 <tyemail@gmail.com>
2022-08-02 17:13:51 +0100

    mobile menue

--- src/layouts/DemoLayout.vue
+++ src/layouts/DemoLayout.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>I am an empty layout used as a place holder and nothinf else</h1>
+  <div class="h-[1000px]">
+    <h1
+      class="flex flex-col justify-center pt-[400px] text-center text-2xl font-bold"
+    >
+      I am an empty layout used as a place holder and nothinf else
+    </h1>
+  </div>
 </template>
--- src/layouts/headerLayout.vue
+++ src/layouts/headerLayout.vue
@@ -49,8 +49,10 @@
       </div>
     </div>
   </header>
+
+  <!-- mobile menu design -->
   <header class="lg:hidden">
-    <div class="flex justify-between px-8">
+    <div class="flex justify-between items-center px-8">
       <div>
         <a href="https://anbenig.com" target="_blank">
           <img
@@ -60,22 +62,31 @@
           />
         </a>
       </div>
-      <div>
-        <button class="lg:hidden">Menu</button>
-
-        <div class="">
-          <div class="" v-for="(item, index) in headerRoutes" :key="index">
-            <RouterLink active-class="active" :to="{ name: item.name }">{{
-              item.title
-            }}</RouterLink>
+      <div class="relative">
+        {{ isMenuOpen }}
+        <button @click="toggleMenu" class="bg-orange-400 p-2">Menu</button>
+        <div
+          class="bg-slate-400 z-10 mt-4 absolute p-10 -ml-5"
+          :class="isMenuOpen ? 'block' : 'hidden'"
+        >
+          <div v-for="(item, index) in headerRoutes" :key="index">
+            <RouterLink
+              @click="toggleMenu"
+              active-class="active"
+              :to="{ name: item.name }"
+              >{{ item.title }}</RouterLink
+            >
           </div>
         </div>
       </div>
     </div>
   </header>
+  <!-- mobile menu design -->
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const headerRoutes = [
   {
     path: "/",
@@ -103,6 +114,13 @@ const headerRoutes = [
     title: "Contact",
   },
 ];
+
+const isMenuOpen = ref(true);
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value;
+  console.log(isMenuOpen.value, "here");
+}
 </script>
 
 <style lang="scss" scoped>
