b924b72865a0e0f91411a264b1bb9aa56e1c006e
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-08-16 14:08:14 +0100

    About us commit

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Background_writeup from "../Sections/background_writeup.vue";
+import Backgroundimage_button from "../Sections/backgroundimage_button.vue";
+import Localcontent from "../Sections/localcontent.vue";
+import Ourleader from "../Sections/ourleader.vue";
+</script>
+
+<template>
+  <main class="relative">
+    <img class="absolute z-10 -mt-1 -ml-1" src="/images/svg/blade-curve.svg" />
+
+    <Background_writeup />
+
+    <Backgroundimage_button></Backgroundimage_button>
+    <Ourleader></Ourleader>
+    <Localcontent></Localcontent>
+  </main>
+</template>
