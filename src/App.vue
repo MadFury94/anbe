72d82dd28012fe441531ebbcbf9f76e079bbfc91
katalyst33 <tyemail@gmail.com>
2022-08-16 22:12:43 +0100

    update app.veu

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterView } from "vue-router";
 import HeaderLayout from "./layouts/headerLayout.vue";
 
-import FooterLayout from "./layouts/FooterLayout.vue";
+import FooterLayout from "./layouts/footerLayout.vue";
 </script>
 
 <template>
