a22c78fcb5c9ddad42fe114ae4644c1200e7b35b
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-08-19 16:11:57 +0100

    Contact Page(UPDATED)
    Contact Page 60% done.

---
+++ src/Sections/ContactOptions.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <section class="grid gap-y-10 px-10 py-20 md:grid-cols-2 lg:grid-cols-3">
+      <div v-for="(item, index) in serviceList" :key="index">
+        <div class="box-border h-48 w-48 items-center gap-x-4 border-4 p-4">
+          <!-- <img
+            :src="`images/icons/${item.icon}`"
+            alt="supply icon"
+            class="w-16"
+          /> -->
+          <h1 class="text-center font-bree text-sm font-bold uppercase">
+            {{ item.title }}
+          </h1>
+          <p class="text-center font-bree text-sm font-light">
+            {{ item.subtitle }}
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup>
+const serviceList = [
+  {
+    // icon: "construction-icon-7-gray.png",
+
+    title: "Our Administrative Office",
+    subtitle: "Road B, House 106, Oba Adeyanka Oyekan Estate, Lekki Phase 1",
+  },
+  {
+    // icon: "construction-icon-5-gray.png",
+
+    title: "Branch Office",
+    subtitle:
+      "LAKESIDE MALL Plot 101 East West Road, Rumudara, Port Harcourt Rivers State",
+  },
+  {
+    // icon: "construction-icon-2-gray.png",
+
+    title: "Branch office",
+    subtitle: "11 MCC Road, Off Ernheren Road, Warri Delta State",
+  },
+];
+</script>
--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { companyInformation } from "../store/store";
 import MiniJumbotron from "../components/MiniJumbotron.vue";
 import ContactJumbotron from "../components/icons/ContactJumbotron.vue";
 import BladeDivider from "../components/BladeDivider.vue";
+import ContactOptions from "../Sections/ContactOptions.vue";
+import ContactArea from "../layouts/ContactArea.vue";
 
 const contactTitle = {
   title: "Contact Us",
@@ -18,14 +21,27 @@ const contactText1 = {
 <template>
   <div class="relative">
     <img class="absolute z-10 -mt-1 -ml-1" src="/images/svg/blade-curve.svg" />
-  </div>
 
-  <MiniJumbotron
-    image="https://anbenig.com/wp-content/uploads/2019/12/handyman_01.jpg"
-    title-class="text-5xl text-orange-500 font-bold font-bree"
-    :title="contactTitle.title"
-    :text="null"
-  />
+    <MiniJumbotron
+      image="https://anbenig.com/wp-content/uploads/2019/12/handyman_01.jpg"
+      title-class="text-5xl text-orange-500 font-bold font-bree"
+      :title="contactTitle.title"
+      :text="null"
+    />
+
+    <div class="">
+      <div class="grid-cols-4 items-center font-bree text-2xl lg:grid">
+        <div></div>
+        <div class="bg-orange-500 py-10 px-20 text-white">Get A Quote</div>
+        <div class="col-span-2 px-20 py-10 text-gray-500">
+          CALL US:
+          <span class="text-orange-500">{{
+            companyInformation.contactNumbers.phone1
+          }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
   <ContactJumbotron
     title-class="text-5xl text-orange-500 font-bold font-bree"
     :title="contactTitle2.title2"
@@ -36,5 +52,7 @@ const contactText1 = {
     Our Fabrication facility is located Omueke village, Igwuruta Rd Ikwerre LGA
     Rivers State. These are our other locations
   </p>
+  <ContactOptions />
+  <ContactArea />
   <BladeDivider />
 </template>
